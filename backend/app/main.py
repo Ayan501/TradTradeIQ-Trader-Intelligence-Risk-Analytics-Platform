97bbe47b21720ca46f4c274bac81e4c0a1d00183
Ayan Mansoori <ayanmansuri535@gmail.com>
2026-06-28 01:46:57 +0530

    add vercel link

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-    ],
+        "https://trad-trade-iq-trader-intelligence-r.vercel.app"
+],
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
