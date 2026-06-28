08bbdb61a89f7f75f000bc7e57397ab206b7e644
Ayan Mansoori <ayanmansuri535@gmail.com>
2026-06-28 15:02:08 +0530

    upadte enviromental variable

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,9 @@
-"""Configuration module."""
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:password@db:5432/TraderIQ"
+load_dotenv()
 
-SECRET_KEY = "tradtradeiq_secret_key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@db:5432/TraderIQ")
+SECRET_KEY = os.getenv("SECRET_KEY", "tradtradeiq_secret_key")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
